6ee7f7717ef48ad01d742076c8d6c05bd505270d
Trung Dinh Quang <trungdq88@gmail.com>
2016-08-17 00:16:20 +0700

    Explain things

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var nodeStatic = require('node-static');
 var http = require('http');
 var socketIO = require('socket.io');
 
+// Serve client static files
 var fileServer = new(nodeStatic.Server)();
 var app = http.createServer(function(req, res) {
   fileServer.serve(req, res);
@@ -10,21 +11,39 @@ var app = http.createServer(function(req, res) {
 
 var io = socketIO.listen(app);
 
+// Alice socket object
 var alice;
+
+// List of Bobs {key: value}
+// - key: string of bob name (socket id)
+// - value: Socket
 var bobList= {};
 
 io.sockets.on('connection', function(socket) {
-  // Wait for Alice
+
+  // 1. Alice come
   socket.on('i-am-alice', function () {
-    alice = socket;
-    console.log('Alice has come!');
+    if (!alice) {
+      alice = socket;
+      console.log('Alice has come!');
+    } else {
+      console.log('New alice has come but ignored because already had one.');
+    }
   });
 
-  // Wait for Bob(s)
+  // 2. Bob come
   socket.on('i-am-bob-and-i-want-to-connect', function () {
     // Give Bob a name
     var bob = 'Bob-' + socket.id;
+
     bobList[bob] = socket;
+
+    // Say sorry if there is no Alice
+    if (!alice) {
+      console.log(bob, ' come but Alice is not around :-(');
+      socket.emit('sorry-alice-is-not-here');
+      return;
+    }
 
     // Tell Alice bob is coming
     alice.emit('bob-is-coming', bob);
@@ -32,11 +51,13 @@ io.sockets.on('connection', function(socket) {
     console.log(bob, ' has come! Told Alice about that.');
   });
 
+  // 3. Alice send Bob an offer
   socket.on('alice-offer-a-session', function (data) {
     console.log('Alice sent an offer to ', data.bobName);
     bobList[data.bobName].emit('alice-offer-a-session', data.sessionDescription);
   });
 
+  // 4. Bob answer the offer
   socket.on('bob-answer-the-offer', function (sessionDescription) {
     // Deliver the answer to Alice
     var bob = 'Bob-' + socket.id;
@@ -47,12 +68,14 @@ io.sockets.on('connection', function(socket) {
     console.log(bob, ' answered the offer. Told Alice about that.');
   });
 
+  // 5. Alice send ICE candidate info
   socket.on('alice-sending-ice-candidate', function (data) {
     // Deliver the candidate to all Bobs
     console.log('Alice sent an ICE candidate to ', data.bobName);
     bobList[data.bobName].emit('alice-sending-ice-candidate', data.candidate);
   });
 
+  // 6. Bob send ICE candidate info
   socket.on('bob-sending-ice-candidate', function (candidate) {
     // Deliver the candidate to Alice
     var bob = 'Bob-' + socket.id;
@@ -62,20 +85,26 @@ io.sockets.on('connection', function(socket) {
     });
     console.log(bob, ' sent an ICE candidate. Delivered that to Alice.');
   });
-});
 
-io.sockets.on('connection', function(socket) {
-  // Who has just disconnected?
-  if (alice && socket.id === alice.id) {
-    // TODO: Alice is tired, game over
-    return;
-  }
+  // 7. FIN
+  socket.on('disconnect', function () {
+    // Who has just disconnected?
+    if (alice && socket.id === alice.id) {
+      // Alice left :-(
+      alice = null;
+      io.emit('alice-left');
+      console.log('Alice left :-(')
+      return;
+    }
 
-  if (bobList[socket.id]) {
-    // Bob left, tell alice
     var bob = 'Bob-' + socket.id;
-    alice.emit('bob-is-leaving', bob);
-  }
+    if (bobList[bob]) {
+      // Bob left, tell Alice
+      alice.emit('bob-is-leaving', bob);
+      console.log(bob, ' left :-(')
+    }
+  });
 });
+
 
 console.log('Server is on, waiting for Alice and Bob(s)');
